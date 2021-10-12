f8c8dc4d5b620c18cd6faa8db5b432e6ab803e16
danra <danra@LAPTOP-KALG3K2S>
2021-10-12 13:27:25 -0700

    fxml docs and login pane

--- CSE360Team7/src/Doctor.java
+++ CSE360Team7/src/Doctor.java
@@ -1,3 +1,7 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 
 public class Doctor {
 
---
+++ CSE360Team7/src/TermProj.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.IOException;
+	
+public class TermProj extends Application {
+
+	   private final int WIDTH = 550;
+	   private final int HEIGHT = 400;
+	   private static Stage stage;
+
+	   public void start(Stage primaryStage) throws Exception
+	   {
+	      stage = primaryStage;
+		  Parent root = FXMLLoader.load(getClass().getResource("LoginPane.fxml"));
+		  primaryStage.setTitle("Pediatric Office System");
+		  primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+	      primaryStage.show(); // Display the stage
+	   }
+	   
+	   
+	   public void changeScene(String fxml) throws IOException {
+		   Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+		   stage.getScene().setRoot(pane);
+	   }
+	   
+	   public static void main(String[] args)
+	   {
+	      Application.launch(args);
+	   }
+	
+
+}
