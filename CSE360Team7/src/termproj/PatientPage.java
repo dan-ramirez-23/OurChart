74546d50ab0017f946a0c4954611f77ff252f539
hannahkaufman <hannahkaufman@192.168.0.12>
2021-11-01 15:23:47 -0700

    I honestly don't know hahah.. ahaha...

--- CSE360Team7/src/termproj/PatientPage.java
+++ CSE360Team7/src/termproj/PatientPage.java
@@ -1,5 +1,11 @@
 package termproj;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import java.io.IOException;
+import javafx.scene.control.Button;
+
 public class PatientPage {
 
+	
 }
