eb76bd498a39a3756a69fd85f3bfba98767c27a5
Totoro <Totoro@192.168.0.201>
2021-11-01 20:35:05 -0700

    Added getters and setters for patient class

--- CSE360Team7/src/termproj/Patient.java
+++ CSE360Team7/src/termproj/Patient.java
@@ -56,4 +56,152 @@ public class Patient extends User {
 		healthConcerns = hc;
 
 	}
+
+	// setters
+
+	public void setDoctor(int Doc) {
+		Doctor = Doc;
+	}
+
+	public void setPhoneNum(int phoneNum) {
+		phoneNumber = phoneNum;
+	}
+
+	public void setEmail(String Email) {
+		email = Email;
+	}
+
+	public void setPharm(String Pharm) {
+		pharmacy = Pharm;
+	}
+
+	public void setInsurer(String Insurer) {
+		insurer = Insurer;
+	}
+
+	public void setImmunizations(String[] immun) {
+		immunizations = immun;
+	}
+
+	public void setPrescriptions(String[] P) {
+		prescriptions = P;
+	}
+
+	public void setMedications(String[] Meds) {
+		medications = Meds;
+	}
+
+	public void setHealthIssues(String[] hi) {
+		healthIssues = hi;
+	}
+
+	public void setExamFindings(String[] ef) {
+		examFindings = ef;
+	}
+
+	public void setRecommendations(String rec) {
+		recommendations = rec;
+	}
+
+	public void setWeight(double w) {
+		weight = w;
+	}
+
+	public void setHeight(double h) {
+		height = h;
+	}
+
+	public void setBodyTemp(double bt) {
+		bodyTemp = bt;
+	}
+
+	public void setBloodPress(double bp) {
+		bloodPressure = bp;
+	}
+
+	public void setAllergies(String[] aller) {
+		allergies = aller;
+	}
+
+	public void setHealthConcerns(String[] hc) {
+		healthConcerns = hc;
+	}
+
+	// getters
+
+	public int getPatientID() {
+		return patientID;
+	}
+
+	public int getDoctor() {
+		return Doctor;
+	}
+
+	public String getDOB() {
+		return dob;
+	}
+
+	public int getPhoneNum() {
+		return phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPharmacy() {
+		return pharmacy;
+	}
+
+	public String insurer() {
+		return insurer;
+	}
+
+	public String[] getImmunizations() {
+		return immunizations;
+	}
+
+	public String[] getPrescriptions() {
+		return prescriptions;
+	}
+
+	public String[] getMedications() {
+		return medications;
+	}
+
+	public String[] getHealthIssues() {
+		return healthIssues;
+	}
+
+	public String[] getExamFindings() {
+		return examFindings;
+	}
+
+	public String getRecs() {
+		return recommendations;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public double getBodyTemp() {
+		return bodyTemp;
+	}
+
+	public double getBloodPress() {
+		return bloodPressure;
+	}
+
+	public String[] getAllergies() {
+		return allergies;
+	}
+
+	public String[] getHealthConcerns() {
+		return healthConcerns;
+	}
 }
