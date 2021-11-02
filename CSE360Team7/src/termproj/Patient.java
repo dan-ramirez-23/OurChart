0c4a97296b4136fa272ade760ba81fe92f1752bf
sebas <sebas@host.docker.internal>
2021-11-01 19:08:14 -0700

    constructor for User and Patient

--- CSE360Team7/src/termproj/Patient.java
+++ CSE360Team7/src/termproj/Patient.java
@@ -1,8 +1,8 @@
 package termproj;
 
 public class Patient extends User {
-	private String firstName;
-	private String lastName;
+	//private String firstName; contained in User class
+	//private String lastName;
 	private int patientID;
 	private int Doctor;
 	private String dob;
@@ -12,9 +12,21 @@ public class Patient extends User {
 	private String insurer;
 	private String[] immunizations;
 	private String[] prescriptions;
-	private PatientMessage[] inbox;
+	//private PatientMessage[] inbox;
 	
 	public Patient() {
 		
+	}
+	public Patient(String fName, String lName, int id, int doc, String date, int pNum, String em, String pharm, String ins, String[] imm, String[] pres, PatientMessage[] ib) {
+		super(fName, lName, ib);
+		patientID= id;
+		Doctor = doc;
+		dob = date;
+		phoneNumber = pNum;
+		email = em;
+		pharmacy = pharm;
+		insurer= ins;
+		immunizations = imm;
+		prescriptions = pres;
 	}
 }
