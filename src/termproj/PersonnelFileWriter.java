6e28beac27a2116f6e7b5e88b294fdd572850a89
Totoro <Totoro@192.168.0.201>
2021-11-05 11:58:49 -0700

    Updated personal file writer
    Similar changes to personal file reader

--- src/termproj/PersonnelFileWriter.java
+++ src/termproj/PersonnelFileWriter.java
@@ -4,34 +4,31 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class PersonnelFileWriter {
-	
+
 	private String fName;
-	private ArrayList<User> userList= new ArrayList<>();
-	
+	private ArrayList<User> userList = new ArrayList<>();
+
 	public PersonnelFileWriter() {
-		
+
 	}
-	public PersonnelFileWriter(ArrayList<User> uL) {
-		userList = uL;
-	}
-	
-	
-	
+
+	// public PersonnelFileWriter(ArrayList<User> uL) {
+	// userList = uL;
+	// }
+
 	public void writePatient() {
-		
+
 	}
-	
-	
+
 	public void writeUser(User usr) {
 		try {
 			fName = "userdata//" + usr.getUsername() + ".txt";
 			System.out.print(fName);
-			//URL url = getClass().getResource(fName);
-			//System.out.print(url);
+			// URL url = getClass().getResource(fName);
+			// System.out.print(url);
 			File empFile = new File(fName);
 			FileOutputStream f = new FileOutputStream(empFile);
 			ObjectOutputStream o = new ObjectOutputStream(f);
@@ -40,10 +37,35 @@ public class PersonnelFileWriter {
 
 			o.close();
 			f.close();
-			
-		}  catch (IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void writeUserToSingleFile(User usr, ArrayList<User> ul) {
+		try {
+			// fName = "userdata//" + usr.getUsername() + ".txt";
+			// System.out.print(fName);
+			// URL url = getClass().getResource(fName);
+			// System.out.print(url);
+			File directory = new File("userData");
+			if (!directory.exists()) {
+				directory.mkdir();
+			}
+			File empFile = new File("userdata//users.txt");
+			FileOutputStream f = new FileOutputStream(empFile);
+			ObjectOutputStream o = new ObjectOutputStream(f);
+
+			ul.add(usr);
+			o.writeObject(ul);
+
+			o.close();
+			f.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
