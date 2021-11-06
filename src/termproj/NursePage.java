f8cead36d72d4831cd086d03ab973ecf22664649
hannahkaufman <hannahkaufman@192.168.0.129>
2021-11-06 00:22:31 -0700

    Hardcoding Doctors & Nurses

--- src/termproj/NursePage.java
+++ src/termproj/NursePage.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -98,6 +99,7 @@ public class NursePage extends Pages{
 		newiD(pat);
 		setLoginInfo(pat);
 		assignPatient(pat);
+		
 	}
 	
 	public void newiD(Patient pat) {
@@ -115,6 +117,7 @@ public class NursePage extends Pages{
 	}
 	
 	public void assignPatient(Patient pat) {
+		
 		System.out.println("Mr. " + pat.lastName + " is in the system. His iD is: " + pat.getPatientID());
 	}
 	/*@Override
--- src/termproj/TermProj.java
+++ src/termproj/TermProj.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import java.io.IOException;
+import java.util.ArrayList;
 	
 public class TermProj extends Application {
 
@@ -48,6 +49,17 @@ public class TermProj extends Application {
 	   {
 	      Application.launch(args);
 	   }
-	
+	   
+	   public void hardcode() {
+		   ArrayList <User> doctorList, userList;
+		   doctorList = new ArrayList <User> ();
+		   userList = new ArrayList <User> ();
+		   Doctor doc = new Doctor("Hannah", "Kaufman", "hjkaufma", "password", 102, int[] patients);
+		   Doctor doc = new Doctor("Jackson", "Carrion", "jtcarrio", "password", 900, int[] patients);
+		   Doctor doc = new Doctor("Dan", "Ramirez", "darami14", "password", 032, int[] patients);
+		   Nurse nur = new Nurse("Audrey", "Wong", "awong24", "password", 293, int[] patients);
+		   Nurse nur = new Nurse("Sebastian", "Aguilar", "sdiazagu", "password", 289, int[] patients);
+		  
+	   }
 
 }
