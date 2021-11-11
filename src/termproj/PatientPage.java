93940b2a3b20dff4a4d41a8fa245b04269896a2c
Totoro <Totoro@192.168.0.201>
2021-11-10 19:13:36 -0700

    Created fx id's for fields

--- src/termproj/PatientPage.java
+++ src/termproj/PatientPage.java
@@ -1,34 +1,23 @@
 package termproj;
 
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javafx.scene.control.Button;
-
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+public class PatientPage extends Pages {
+	// private ArrayList<User> userList;
 
-public class PatientPage extends Pages{
-	//private ArrayList<User> userList;
-	
 	public PatientPage(String un, ArrayList<User> uL) {
 		super(un, uL);
 		System.out.println("On patient page creation, un = " + un);
 	}
-	
+
 	private String username;
-	
+
 	@FXML
 	private Button sendMsgButton;
 	@FXML
@@ -36,28 +25,26 @@ public class PatientPage extends Pages{
 	@FXML
 	private TextField subject;
 
-	
 	@FXML
-	public void initialize() {
-		
+	public void initialize() { // start here
 
 	}
-	
-	
+
 	public void send(ActionEvent event) throws IOException {
 		sendMsg();
 	}
-	
+
 	public void sendMsg() {
-		
-		Patient pat = new Patient("bill", "hicks", 27, "August 13, 1974", "bhicks.xyz", "508888888", "todd","Blue Cross"); 
+
+		Patient pat = new Patient("bill", "hicks", 27, "August 13, 1974",
+				"bhicks.xyz", "508888888", "todd", "Blue Cross");
 		pat.setUserName("bhicks");
 		pat.setPassword("apple");
 		PersonnelFileWriter pfw = new PersonnelFileWriter();
 		pfw.writeUser(pat);
-		
+
 		String subj = subject.getText();
-		System.out.println("in the sendMsg function - subj is: "+ subj);
+		System.out.println("in the sendMsg function - subj is: " + subj);
 		String body = messageBody.getText();
 		String senderUN = username;
 
@@ -66,5 +53,4 @@ public class PatientPage extends Pages{
 		msg.sendMessage();
 	}
 
-	
 }
